b0aff2e929fa63fe5542d98f11de54a8f3d1efc5
erdder1103-art <erdder1103@gmail.com>
2026-03-11 21:06:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.static("public"));
 
